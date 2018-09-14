a7caf5cf1ec8e7024f85f37941c93c7d9175d54e
Erlend Fagerheim <erlendf80@gmail.com>
2018-09-14 15:52:24 +0200

    use WIN_HIGHLIGHT event

--- vis-title.lua
+++ vis-title.lua
@@ -1,3 +1,3 @@
-vis.events.subscribe(vis.events.WIN_OPEN, function(win)
+vis.events.subscribe(vis.events.WIN_HIGHLIGHT, function(win)
 	vis:command(string.format(":!echo -ne '\\033]0;%s\\007'", win.file.name))
 end)
