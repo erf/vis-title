d05590272714570b5114f8c23fea43c4141c1cc9
erf <erlendf80@gmail.com>
2019-02-18 10:55:35 +0100

    Set title on new file and update title on save

--- title.lua
+++ title.lua
@@ -1,3 +1,13 @@
+function setTitle (title)
+	vis:command(string.format(":!echo -ne '\\033]0;%s\\007'", title))
+end
+
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	vis:command(string.format(":!echo -ne '\\033]0;%s\\007'", win.file.name))
+    local name = win.file.name
+	if name == nil then name = '[No Name]' end
+	setTitle(name)
+end)
+
+vis.events.subscribe(vis.events.FILE_SAVE_POST, function(file, path)
+	setTitle(file.name)
 end)
