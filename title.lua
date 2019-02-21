eba4a0b553edd9f74a07a2d9dae01e3e2ce24d59
Erlend Fagerheim <erlendf80@gmail.com>
2019-02-21 20:23:43 +0100

    use underscore for function name

--- title.lua
+++ title.lua
@@ -1,11 +1,11 @@
-function setTitle (title)
+function set_title(title)
 	vis:command(string.format(":!echo -ne '\\033]0;%s\\007'", title))
 end
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	setTitle(win.file.name or '[No Name]')
+	set_title(win.file.name or '[No Name]')
 end)
 
 vis.events.subscribe(vis.events.FILE_SAVE_POST, function(file, path)
-	setTitle(file.name)
+	set_title(file.name)
 end)
