c3f4a75093ebaf06c572eac3dbc319f3fe8a208e
Erlend Fagerheim <erlendf80@gmail.com>
2020-02-10 00:19:02 +0100

    roll back lua print which caused bug

--- init.lua
+++ init.lua
@@ -1,14 +1,14 @@
 function set_title(title)
-	print(string.format('\27]0;%s\7', title))
+	vis:command(string.format(":!echo -ne '\\033]0;%s\\007'", title))
 end
 
 vis.events.subscribe(vis.events.WIN_OPEN, function(win)
-	print('\27[22t')
+	vis:command(":!echo -ne '\\033[22t'")
 	set_title(win.file.name or '[No Name]')
 end)
 
 vis.events.subscribe(vis.events.WIN_CLOSE, function(win)
-	print('\27[23t')
+	vis:command(":!echo -ne '\\033[23t'")
 end)
 
 vis.events.subscribe(vis.events.FILE_SAVE_POST, function(file, path)
